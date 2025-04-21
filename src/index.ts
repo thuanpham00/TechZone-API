3604fd45e41b3fc318f8f7693307f905e6c7bc09
Pham Thuan <phamminhthuan912@gmail.com>
2025-04-21 17:29:21 +0700

    add url client deploy

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.use(express.json()) // biến request từ object thành json
 app.use(cookieParse())
 app.use(
   cors({
-    origin: ["http://localhost:3500", "http://localhost:4173"], // URL client
+    origin: ["http://localhost:3500", "http://localhost:4173", "https://tech-zone-shop.vercel.app/"], // URL client
     credentials: true // Cho phép gửi cookie lên client
   })
 )
